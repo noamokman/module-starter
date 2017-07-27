014fcad137129ddfb257605a25fd8aeb60dea67c
Noam Okman <noamokman@gmail.com>
2017-07-27 08:10:05 +0300

    feat(cli): used caporal for the cli

--- src/cli.js
+++ src/cli.js
@@ -1,36 +1,30 @@
 import {resolve} from 'path';
-import program from 'commander';
+import program from 'caporal';
 import pkg from '../package.json';
 import updateNotifier from 'update-notifier';
 const moduleStarter = require('./');
 const notifier = updateNotifier({pkg});
 
 program.version(pkg.version)
-  .description('Module starter cli tool')
-  .usage('[options] [path]')
-  .option('-c --cli', 'Initialize a CLI Tool module');
+  .description(pkg.description)
+  .argument('[path]', 'directory to initialize', null, process.cwd())
+  .option('-c, --cli', 'Initialize a CLI Tool module', program.BOOL, false)
+  .action(({path}, {cli}, logger) => {
+    moduleStarter.initilizeModuleDirectory({path: resolve(path), cli})
+      .then(() => {
+        logger.info('All done!');
+
+        notifier.notify();
+      })
+      .catch(logger.error);
+  });
 
 /**
  * Handle cli arguments
  *
  * @param {string[]} argv - string array of the arguments
- * @returns {void}
  */
 export default argv => {
   program
     .parse(argv);
-
-  let path = process.cwd();
-
-  if (program.args.length) {
-    path = resolve(program.args.shift());
-  }
-
-  return moduleStarter.initilizeModuleDirectory({path, cli: program.cli})
-    .then(() => {
-      console.log('All done!');
-
-      notifier.notify();
-    })
-    .catch(console.log);
 };
